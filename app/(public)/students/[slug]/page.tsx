3227ba8e65f1f3123f4b9ac83f3a226a1229b0dd
Brandon <brandnlholl@gmail.com>
2024-04-27 16:26:40 -0700

    More Updates
    Co-authored-by: Lukas <quick007@users.noreply.github.com>

--- app/(public)/students/[slug]/page.tsx
+++ app/(public)/students/[slug]/page.tsx
@@ -1,11 +1,20 @@
 export default function Student({
-    params: { slug },
-  }: {
-    params: { slug: string };
-  }) {
-    return (
-        <div>
-            <h1>Student: {slug}</h1>
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
+  return (
+    <>
+      <div className="w-full bg-slate-300 h-64 flex items-end justify-center">
+        <div className="max-w-screen-lg w-full bg-slate-800 h-24 flex items-center relative">
+          <div className="h-44 w-44 bg-slate-500 rounded-full bottom-0 left-0"></div>
+          <div className="ml-4">
+            <h1 className="text-2xl font-bold text-white">student.full_name</h1>
+            <p className="text-white">student.class</p>
+          </div>
         </div>
-    )
-    }
+      </div>
+      <div className="max-w-screen-lg mx-auto flex">test</div>
+    </>
+  );
+}
--- app/dashboard/standley/layout.tsx
+++ app/dashboard/standley/layout.tsx
@@ -2,11 +2,21 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
-		<div className="flex">
-			<nav className="w-72">
-
+		<div className="flex h-screen">
+			<nav className="w-72 p-8 grid gap-8 border-r">
+				<section></section>
+				<section></section>
+				<section className="mt-auto"></section>
 			</nav>
 			{children}
+			<div>
+				<section>
+
+				</section>
+				<section>
+
+				</section>
+			</div>
 		</div>
 	)
 }
