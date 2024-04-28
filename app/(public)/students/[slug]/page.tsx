55b229ce95407f4e664875f0198011c7f9c51c19
Brandon <brandnlholl@gmail.com>
2024-04-28 16:11:39 -0700

    Refactor student page layout and update next.config

--- app/(public)/students/[slug]/page.tsx
+++ app/(public)/students/[slug]/page.tsx
@@ -30,42 +30,48 @@ export default function Student({
         </div>
       </div>
       <div className="max-w-screen-xl mx-auto flex">
-        <div className="flex-1 flex-col py-12">
-          <h1 className="font-bold text-4xl">Alexis Lo</h1>
-          <h2 className="text-xl">2025</h2>
-          <p className="py-4">
-            Hi, my name is Alexis Lo, and I am a junior at SHC. Academically, my
-            favorite subjects are math and science. Something about math is the
-            satisfaction of getting a problem right and knowing that you
-            achieved something through that long process. Solving math problems
-            is something that I enjoy the challenge of, so math has been one of
-            my favorite subjects since elementary school. On the other hand, I
-            enjoy science because I like being able to understand the world
-            around me. This year, I am building from my classes last year to
-            take 4 APs (AP Computer Science A, AP Mandarin, AP Language and
-            Composition, and AP Calculus AB), 1 Honors (Biology 1,2H), along
-            with Ethics and U.S. History 1,2. I’m also a part of a bunch of
-            clubs here at SHC, including Tech Crew (lighting) and being a leader
-            in the Photography Club. Outside of school, I currently take ballet
-            classes about four times a week. Over the past year, it has become
-            an 8-10 hours-a-week commitment. Dancing has taught me several
-            skills and life lessons that everyone should learn in their life,
-            such as discipline and diligence. Being in i2 has given me so many
-            valuable learning experiences. Through the passion projects the
-            program has allowed me to pursue, I have learned so much about
-            programming in different languages and gained better time management
-            and persistence. I also look forward to the skills I can gain this
-            upcoming year!
-          </p>
-          <div className="flex">
-            <div className="flex-1">
-              <h2 className="text-2xl font-semibold text-emerald-900">Blogs</h2>
+        <div className="flex py-12">
+          <div className="flex-1 flex-col ">
+            <h1 className="font-bold text-4xl">Alexis Lo</h1>
+            <h2 className="text-xl">2025</h2>
+            <p className="py-8 pr-8 flex-1">
+              Hi, my name is Alexis Lo, and I am a junior at SHC. Academically,
+              my favorite subjects are math and science. Something about math is
+              the satisfaction of getting a problem right and knowing that you
+              achieved something through that long process. Solving math
+              problems is something that I enjoy the challenge of, so math has
+              been one of my favorite subjects since elementary school. On the
+              other hand, I enjoy science because I like being able to
+              understand the world around me. This year, I am building from my
+              classes last year to take 4 APs (AP Computer Science A, AP
+              Mandarin, AP Language and Composition, and AP Calculus AB), 1
+              Honors (Biology 1,2H), along with Ethics and U.S. History 1,2. I’m
+              also a part of a bunch of clubs here at SHC, including Tech Crew
+              (lighting) and being a leader in the Photography Club. Outside of
+              school, I currently take ballet classes about four times a week.
+              Over the past year, it has become an 8-10 hours-a-week commitment.
+              Dancing has taught me several skills and life lessons that
+              everyone should learn in their life, such as discipline and
+              diligence. Being in i2 has given me so many valuable learning
+              experiences. Through the passion projects the program has allowed
+              me to pursue, I have learned so much about programming in
+              different languages and gained better time management and
+              persistence. I also look forward to the skills I can gain this
+              upcoming year!
+            </p>
+
+            <div className="flex">
+              <div className="flex-1">
+                <h2 className="text-2xl font-semibold text-emerald-900">
+                  Blogs
+                </h2>
+              </div>
             </div>
-            <div className="flex-1">
-              <h2 className="text-2xl font-semibold text-emerald-900">
-                Projects
-              </h2>
-            </div>
+          </div>
+          <div className="w-1/3">
+            <h2 className="text-2xl font-semibold text-emerald-900">
+              Projects
+            </h2>
           </div>
         </div>
       </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com, i2.today"],
+    domains: ["images.unsplash.com", "i2.today"],
   },
 };
 
